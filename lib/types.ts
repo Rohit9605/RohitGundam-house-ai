becc50b6542f2bb236c7e82f4743779ad3895bef
Rohit Gundam <123128359+Rohit9605@users.noreply.github.com>
2026-09-24 17:55:38 -0500

    Add lib/types.ts

---
+++ lib/types.ts
@@ -0,0 +1,26 @@
+export type Room = {
+  name: string;
+  x: number;
+  z: number;
+  width: number;
+  depth: number;
+  floor: number;
+};
+
+export type HouseDesign = {
+  title: string;
+  analysis: {
+    style: string;
+    massing: string;
+    roof: string;
+    materials: string[];
+    windows: string;
+    signatureFeatures: string[];
+  };
+  concept: string;
+  width: number;
+  depth: number;
+  floors: number;
+  ceilingHeight: number;
+  rooms: Room[];
+};
